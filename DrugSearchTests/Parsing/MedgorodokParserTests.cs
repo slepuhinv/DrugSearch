a791a7f6d5333c6920280c1d344e32885d43fac4
Slepuhin Vladimir <slepuhin.v@gmail.com>
2017-03-14 20:13:00 +0500

    Refactor

--- DrugSearchTests/Parsing/MedgorodokParserTests.cs
+++ DrugSearchTests/Parsing/MedgorodokParserTests.cs
@@ -16,19 +16,21 @@ namespace DrugSearch.Parsing.Tests
         {
             var html = @"
 <tr class=""apothecas-addresses-list-item"" data-district=""3"" data-worktime=""0"" style=""display: table-row;"">
-                <td class=""apothecas-addresses-list-item-name"">
-                    <div class=""apothecas-addresses-list-item-name-field""><a href=""/enterprise/apteka-a2-krestinskogo-59-1-enterprise-2055263.html"">Аптека А2 (Крестинского 59/1)</a></div>
-                    <div class=""apothecas-addresses-list-item-name-time"">
-                                        <img alt="""" src=""/assets/img/icons/time-green.png""> 08:00-23:00
-                    </div>
-                </td>
-                <td class=""apothecas-addresses-list-item-contacts"">
-                    <div class=""apothecas-addresses-list-item-contacts-address"">ул. Крестинского, д. 59/1</div>
-                    <div class=""apothecas-addresses-list-item-contacts-phones"">+7 912 204-00-03</div>
-                </td>
-                <td class=""apothecas-addresses-list-item-update"" title=""1"">09-03-2017</td>
-                <td class=""apothecas-addresses-list-item-price""><span>248.00</span> р.</td>
-            </tr>
+    <td class=""apothecas-addresses-list-item-name"">
+        <div class=""apothecas-addresses-list-item-name-field"">
+            <a href=""/enterprise/apteka-a2-krestinskogo-59-1-enterprise-2055263.html"">Аптека А2 (Крестинского 59/1)</a>
+        </div>
+        <div class=""apothecas-addresses-list-item-name-time"">
+            <img alt="""" src=""/assets/img/icons/time-green.png""> 08:00-23:00
+        </div>
+    </td>
+    <td class=""apothecas-addresses-list-item-contacts"">
+        <div class=""apothecas-addresses-list-item-contacts-address"">ул. Крестинского, д. 59/1</div>
+            <div class=""apothecas-addresses-list-item-contacts-phones"">+7 912 204-00-03</div>
+    </td>
+    <td class=""apothecas-addresses-list-item-update"" title=""1"">09-03-2017</td>
+    <td class=""apothecas-addresses-list-item-price""><span>248.00</span> р.</td>
+</tr>
 ";
             var aptekaInfo = MedgorodokParser.Parse(html);
 
